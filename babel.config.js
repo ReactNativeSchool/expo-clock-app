e1209255f7a6db278b4361b096f207aed267b7f6
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-05-31 10:14:53 -0500

    Setup path resolving in ts and babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            // This needs to be mirrored in tsconfig.json
+            src: "./src",
+            components: "./src/components",
+            constants: "./src/constants",
+            hooks: "./src/hooks",
+            navigation: "./src/navigation",
+            screens: "./src/screens",
+          },
+        },
+      ],
+    ],
   };
 };
