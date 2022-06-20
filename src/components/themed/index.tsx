e99e808188b084716f166162b9a660c9821e0440
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-06-20 16:00:02 -0500

    Forgot to export a type

--- src/components/themed/index.tsx
+++ src/components/themed/index.tsx
@@ -1,3 +1,3 @@
 export { Text, TextProps } from "./Text";
-export { View, SafeAreaView } from "./View";
+export { View, SafeAreaView, ViewProps } from "./View";
 export { StatusBar, StatusBarProps } from "./StatusBar";
