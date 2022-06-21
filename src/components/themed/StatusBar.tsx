30aefc289e03b031269e7b7d24db7025cb2bd7d0
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-06-21 13:42:24 -0500

    Better android statusbar

--- src/components/themed/StatusBar.tsx
+++ src/components/themed/StatusBar.tsx
@@ -3,9 +3,15 @@ import { StatusBar as DefaultStatusBar, StatusBarProps } from "react-native";
 import { useThemeColors } from "hooks/useThemeColors";
 
 export const StatusBar = (props: StatusBarProps) => {
-  const { isDark } = useThemeColors();
+  const { isDark, colors } = useThemeColors();
 
   const barStyle = isDark ? "light-content" : "dark-content";
 
-  return <DefaultStatusBar barStyle={barStyle} {...props} />;
+  return (
+    <DefaultStatusBar
+      barStyle={barStyle}
+      backgroundColor={colors.background}
+      {...props}
+    />
+  );
 };
