e58e361ac5b7bdd305a126a3f04a1765c77a46a5
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-07-14 10:25:17 -0500

    Count up from a point in time

--- src/hooks/useStopWatch.ts
+++ src/hooks/useStopWatch.ts
@@ -5,34 +5,60 @@ export type LapData = {
   lap: number;
 };
 
+const padStart = (num: number) => {
+  return num.toString().padStart(2, "0");
+};
+
+// const SAMPLE_START = 1000 + 543; // 1.543 seconds 00:01.54
+// const SAMPLE_START = 1000 * 60 + 1000 * 2 + 543; // 1 minute 2.543 seconds 01:02.54
+// const SAMPLE_START = 1000 * 60 * 32 + 1000 * 2 + 543; // 32 minute 2.543 seconds 32:02.54
+// const SAMPLE_START = 1000 * 60 * 60 * 2 + 1000 * 60 * 32 + 1000 * 2 + 543; // 2 hours 32 minute 2.543 seconds 02:32:02.54
+// const SAMPLE_START = 1000 * 60 * 60 * 200 + 1000 * 60 * 32 + 1000 * 2 + 543; // 200 hours 32 minute 2.543 seconds 200:32:02.54
 const formatMs = (milliseconds: number) => {
-  const ms = Math.floor(milliseconds % 60);
-  const prettyMs = ms < 10 ? `0${ms}` : ms;
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
 
-  const seconds = Math.floor((milliseconds / 60) % 60);
-  const prettySeconds = seconds < 10 ? `0${seconds}` : seconds;
+  // using the modulus operator gets the remainder if the time roles over
+  // we don't do this for hours because we want them to rollover
+  // seconds = 81 -> minutes = 1, seconds = 21.
+  // 60 minutes in an hour, 60 seconds in a minute, 1000 milliseconds in a second.
+  minutes = minutes % 60;
+  seconds = seconds % 60;
+  // divide the milliseconds by 10 to get the tenths of a second. 543 -> 54
+  const ms = Math.floor((milliseconds % 1000) / 10);
 
-  const minutes = Math.floor((milliseconds / 60 / 60) % 24);
-  const prettyMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  let str = `${padStart(minutes)}:${padStart(seconds)}.${padStart(ms)}`;
 
-  return `${prettyMinutes}:${prettySeconds}:${prettyMs}`;
+  if (hours > 0) {
+    str = `${padStart(hours)}:${str}`;
+  }
+
+  return str;
 };
 
 export const useStopWatch = () => {
+  // const [startTime, setStartTime] = useState<number>(Date.now());
   const [time, setTime] = useState(0);
+  const [timeWhenLastStopped, setTimeWhenLastStopped] = useState<number>(0);
   const [isRunning, setIsRunning] = useState(false);
   const interval = useRef<ReturnType<typeof setInterval>>();
-  const [laps, setLaps] = useState<number[]>([]);
+  // const [laps, setLaps] = useState<number[]>([]);
 
   const start = () => {
     setIsRunning(true);
+    const startTime = Date.now();
+    // setStartTime(newStartTime);
     interval.current = setInterval(() => {
-      setTime((time) => time + 1);
+      // if (startTime) {
+      setTime(() => Date.now() - startTime + timeWhenLastStopped);
+      // }
     }, 1);
   };
 
   const stop = () => {
     setIsRunning(false);
+    setTimeWhenLastStopped(time);
 
     if (interval.current) {
       clearInterval(interval.current);
@@ -43,41 +69,47 @@ export const useStopWatch = () => {
   const reset = () => {
     setIsRunning(false);
     setTime(0);
-    setLaps([]);
+    setTimeWhenLastStopped(0);
+    // setLaps([]);
+    // setStartTime(Date.now());
     if (interval.current) {
+      clearInterval(interval.current);
       interval.current = undefined;
     }
   };
 
   const lap = () => {
-    setLaps((laps) => [time, ...laps]);
+    // setLaps((laps) => [time, ...laps]);
   };
 
   let slowestLapTime: number | undefined;
   let fastestLapTime: number | undefined;
-  const formattedLapData = laps.map((l, index) => {
-    const previousLap = laps[index + 1] || 0;
-    const lapTime = l - previousLap;
+  // const formattedLapData = laps.map((l, index) => {
+  //   const previousLap = laps[index + 1] || 0;
+  //   const lapTime = l - previousLap;
 
-    if (!slowestLapTime || lapTime > slowestLapTime) {
-      slowestLapTime = lapTime;
-    }
+  //   if (!slowestLapTime || lapTime > slowestLapTime) {
+  //     slowestLapTime = lapTime;
+  //   }
 
-    if (!fastestLapTime || lapTime < fastestLapTime) {
-      fastestLapTime = lapTime;
-    }
+  //   if (!fastestLapTime || lapTime < fastestLapTime) {
+  //     fastestLapTime = lapTime;
+  //   }
 
-    return {
-      time: formatMs(lapTime),
-      lap: laps.length - index,
-    };
-  });
+  //   return {
+  //     time: formatMs(lapTime),
+  //     lap: laps.length - index,
+  //   };
+  // });
 
+  console.log();
   return {
     // Data
     time: formatMs(time),
-    currentLapTime: laps[0] ? formatMs(time - laps[0] || 0) : formatMs(time),
-    laps: formattedLapData,
+    // currentLapTime: laps[0] ? formatMs(time - laps[0] || 0) : formatMs(time),
+    currentLapTime: formatMs(time),
+    // laps: formattedLapData,
+    laps: [],
     slowestLapTime: formatMs(slowestLapTime || 0),
     fastestLapTime: formatMs(fastestLapTime || 0),
 
